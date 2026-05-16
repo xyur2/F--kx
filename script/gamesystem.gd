b34af2f28c032dc2522a94e0d855de44f4d1a844
xyur2 <syr221112@gmail.com>
2026-05-16 14:50:23 +0900

    장난 선택 구현 완료

--- script/gamesystem.gd
+++ script/gamesystem.gd
@@ -7,13 +7,17 @@ var bungee_pos = Vector2(3000, 0)
 var tower_pos = Vector2(0, 0)
 var camera
 var customer
+var joke_final
 var me
 var is_waiting = true
 var tower_my_line
 var tower_customer_line
 var bungee_my_line
 var bungee_customer_line
+var joke_scene
+var chosen_button = 0
 signal continued
+signal button_chosen
 
 func _ready():
 	state = "ongame"
@@ -22,7 +26,10 @@ func _ready():
 	tower_customer_line = get_node("Tower/MyTalkbox/TextEdit")
 	bungee_my_line = get_node("Bungee/CustomerTalkbox/TextEdit2")
 	bungee_customer_line = get_node("Bungee/MyTalkbox/TextEdit")
+	joke_scene = get_node("Joke")
+	joke_scene.visible = false
 	print("씬 불러오기 성공")
+	
 
 	# _process 대신 _ready에서 시작
 	if state == "ongame":
@@ -31,7 +38,6 @@ func _ready():
 func start_game():
 	print("게임 시작 성공")
 	today = day.new()
-	customer = Customer.new()
 	today.set_day(1)
 	today.set_goal(0, 2)
 	print("초기 설정 완료")
@@ -50,6 +56,7 @@ func day_process():
 	print("손님 접대 시작")
 	customer_num = today.get_goal_customer()
 	for i in range(customer_num):
+		customer = Customer.new()
 		lines = [
 			["me", "어서오세요! 번지점프 하러 오셨나요?"],
 			["customer", customer.get_line1()],
@@ -62,6 +69,7 @@ func day_process():
 			["me", "물론 저희 번지점프대에선 한번도 사고가 난 적이 없답니다!"],
 			["me", "자, 여기 안전장비에요. 착용하시고 준비가 되면 위로 같이 올라가시죠."]
 		]
+		# ----------- 안내 장면 --------------
 		line_num_total = lines.size()
 		today.increase_customer()
 		camera.offset = tower_pos
@@ -79,7 +87,37 @@ func day_process():
 				print("speaker 지정에 문제 발생")
 			
 			line_num_now += 1
-
+		print("손님 안내 완료")
+		# ----------- 장난 선택 장면 --------------
+		camera.offset = bungee_pos
+		joke_scene.visible = true
+		var joke_1 = joke.new()
+		var joke_2 = joke.new()
+		var joke_3 = joke.new()
+		await button_chosen
+		match chosen_button:
+			1: joke_final = joke_1
+			2: joke_final = joke_2
+			3: joke_final = joke_3
+		lines = joke_final.get_lines()
+		
+		print("장난 선택 완료")
+		# ----------- 접대 장면 --------------
+		line_num_total = lines.size()
+		line_num_now = 0
+		joke_scene.visible = false
+		while line_num_now < line_num_total:
+			await continued
+			var speaker = lines[line_num_now][0]
+			var line_now = lines[line_num_now][1]
+			if speaker == "me":
+				set_line_bungee_me(line_now)
+			elif speaker == "customer":
+				set_line_bungee_customer(line_now)
+			else:
+				print("speaker 지정에 문제 발생")
+			
+			line_num_now += 1
 		print("손님 접대 완료")
 
 func set_line_tower_customer(line):
@@ -98,3 +136,16 @@ func set_line_bungee_me(line):
 func _input(event: InputEvent) -> void:
 	if Input.is_action_just_pressed("continue"):
 		emit_signal("continued")
+
+
+func _on_button_pressed() -> void:
+	chosen_button = 1
+	emit_signal("button_chosen")
+
+func _on_button_2_pressed() -> void:
+	chosen_button = 2
+	emit_signal("button_chosen")
+
+func _on_button_3_pressed() -> void:
+	chosen_button = 3
+	emit_signal("button_chosen")
