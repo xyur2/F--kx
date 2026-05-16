e1a6dc10121739b20506c1823c75b11e26f6815c
xyur2 <syr221112@gmail.com>
2026-05-16 12:11:44 +0900

    대사 출력 시스템 및 기초 프로세스 설정

--- script/customer.gd
+++ script/customer.gd
@@ -2,48 +2,94 @@ extends Node2D
 class_name Customer
 
 var personalities = ["Cower", "Bragger", "Mania", "Master"]
-var shapes = []
+#var shapes = []
 var personality = personalities.pick_random()
-var shape = shapes.pick_random()
+#var shape = shapes.pick_random()
 
 func get_line1():
 	var lines
 	match personality:
 		"Cower":
-			lines = []
+			lines = [
+				"Cower 대사 1-1",
+				"Cower 대사 1-2",
+				"Cower 대사 1-2"
+			]
 		"Bragger":
-			lines = []
+			lines = [
+				"Bragger 대사 1-1",
+				"Bragger 대사 1-2",
+				"Bragger 대사 1-3"
+			]
 		"Mania":
-			lines = []
+			lines = [
+				"Mania 대사 1-1",
+				"Mania 대사 1-2",
+				"Mania 대사 1-3"
+			]
 		"Master":
-			lines = []
+			lines = [
+				"Master 대사 1-1",
+				"Master 대사 1-2",
+				"Master 대사 1-3"
+			]
 	return lines.pick_random()
 
 func get_line2():
 	var lines
-	var ret_var
 	match personality:
 		"Cower":
-			lines = []
+			lines = [
+				"Cower 대사 2-1",
+				"Cower 대사 2-2",
+				"Cower 대사 2-2"
+			]
 		"Bragger":
-			lines = []
+			lines = [
+				"Bragger 대사 2-1",
+				"Bragger 대사 2-2",
+				"Bragger 대사 2-3"
+			]
 		"Mania":
-			lines = []
+			lines = [
+				"Mania 대사 2-1",
+				"Mania 대사 2-2",
+				"Mania 대사 2-3"
+			]
 		"Master":
-			lines = []
+			lines = [
+				"Master 대사 2-1",
+				"Master 대사 2-2",
+				"Master 대사 2-3"
+			]
 	return lines.pick_random()
 
 func get_line3():
 	var lines
-	var ret_var
 	match personality:
 		"Cower":
-			lines = []
+			lines = [
+				"Cower 대사 3-1",
+				"Cower 대사 3-2",
+				"Cower 대사 3-2"
+			]
 		"Bragger":
-			lines = []
+			lines = [
+				"Bragger 대사 3-1",
+				"Bragger 대사 3-2",
+				"Bragger 대사 3-3"
+			]
 		"Mania":
-			lines = []
+			lines = [
+				"Mania 대사 3-1",
+				"Mania 대사 3-2",
+				"Mania 대사 3-3"
+			]
 		"Master":
-			lines = []
+			lines = [
+				"Master 대사 1-1",
+				"Master 대사 1-2",
+				"Master 대사 1-3"
+			]
 	return lines.pick_random()
 	
--- script/gamesystem.gd
+++ script/gamesystem.gd
@@ -6,31 +6,95 @@ var yesterday
 var bungee_pos = Vector2(3000, 0)
 var tower_pos = Vector2(0, 0)
 var camera
+var customer
+var me
+var is_waiting = true
+var tower_my_line
+var tower_customer_line
+var bungee_my_line
+var bungee_customer_line
+signal continued
 
 func _ready():
-	state = "ready"
+	state = "ongame"
 	camera = get_node("Camera2D")
+	tower_my_line = get_node("Tower/CustomerTalkbox/TextEdit2")
+	tower_customer_line = get_node("Tower/MyTalkbox/TextEdit")
+	bungee_my_line = get_node("Bungee/CustomerTalkbox/TextEdit2")
+	bungee_customer_line = get_node("Bungee/MyTalkbox/TextEdit")
+	print("씬 불러오기 성공")
 
-func start():
-	if state == "ready":
-		pass
-	elif state == "ongame":
+	# _process 대신 _ready에서 시작
+	if state == "ongame":
+		start_game()
+
+func start_game():
+	print("게임 시작 성공")
+	today = day.new()
+	customer = Customer.new()
+	today.set_day(1)
+	today.set_goal(0, 2)
+	print("초기 설정 완료")
+	for i in range(1, 15):
+		await day_process()
+		yesterday = today
 		today = day.new()
-		var customer_num
-		
-		today.set_day(1)
-		today.set_goal(0, 2)
-		for days in range(1, 14):
-			# ------- 장사 시작 ----------
-			customer_num = today.get_goal_customer()
-			for i in range(customer_num):
-				today.increase_customer()
-				camera.set_viewport(tower_pos)
-				# 대사 부분
-				camera.set_viewport(bungee_pos)
-			# ------- 장사 끝 ----------
-			yesterday = today
-			today = day.new()
+		print(i, "일차 완료")
+
+func day_process():
+	var customer_num
+	var lines
+	var line_num_total
+	var line_num_now
+
+	print("손님 접대 시작")
+	customer_num = today.get_goal_customer()
+	for i in range(customer_num):
+		lines = [
+			["me", "어서오세요! 번지점프 하러 오셨나요?"],
+			["customer", customer.get_line1()],
+			["me", "일단 여기 안전 서류부터 좀 작성해 주세요."],
+			["customer", customer.get_line2()],
+			["me", "음, 서류 확인했어요! 그럼 간단한 안전교육을 진행해 볼게요."],
+			["me", "뛰어내리는 과정에서 줄이 몸을 감쌀 수 있어요."],
+			["me", "이때 목이 졸리지 않게 머리를 팔로 감싸 주셔야 해요."],
+			["customer", customer.get_line3()],
+			["me", "물론 저희 번지점프대에선 한번도 사고가 난 적이 없답니다!"],
+			["me", "자, 여기 안전장비에요. 착용하시고 준비가 되면 위로 같이 올라가시죠."]
+		]
+		line_num_total = lines.size()
+		today.increase_customer()
+		camera.offset = tower_pos
+		line_num_now = 0
+
+		while line_num_now < line_num_total:
+			await continued
+			var speaker = lines[line_num_now][0]
+			var line_now = lines[line_num_now][1]
+			if speaker == "me":
+				set_line_tower_me(line_now)
+			elif speaker == "customer":
+				set_line_tower_customer(line_now)
+			else:
+				print("speaker 지정에 문제 발생")
 			
-	elif state == "pause":
-		pass
+			line_num_now += 1
+
+		print("손님 접대 완료")
+
+func set_line_tower_customer(line):
+	tower_customer_line.text = line
+	print(line)
+func set_line_tower_me(line):
+	tower_my_line.text = line
+	print(line)
+func set_line_bungee_customer(line):
+	bungee_customer_line.text = line
+	print(line)
+func set_line_bungee_me(line):
+	bungee_my_line.text = line
+	print(line)
+
+func _input(event: InputEvent) -> void:
+	if Input.is_action_just_pressed("continue"):
+		emit_signal("continued")
